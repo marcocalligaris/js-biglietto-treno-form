d55e6462eb147cdd1fb676c1f9b39bdb02a68f40
marcocalligaris <marcocally@gmail.com>
2022-06-22 16:46:17 +0200

    Discount

--- js/script.js
+++ js/script.js
@@ -5,3 +5,30 @@
 // va applicato uno sconto del 40% per gli over 65.
 // Il recap dei dati e l'output del prezzo finale va stampato in pagina (formattato con massimo due decimali, per indicare centesimi sul prezzo).
 
+// * Variabili
+
+const passenger = document.getElementById('name');
+const kms = document.getElementById('kms');
+const ageOption = document.querySelector('.age-option');
+const buttonSend = document.getElementById('send-data');
+const buttonReset = document.getElementById('reset');
+const ticket = document.querySelector('.ticket');
+
+
+// * Calcolo il prezzo standard del viaggio
+
+let ticketPrice = parseInt(kms * 0.21);
+
+// * Calcolo gli sconti
+
+if (ageOption.value('junior')) {
+    ticketPrice *= 0.8;
+}
+
+else if (ageOption.value('senior')) {
+    ticketPrice *= 0.6;
+}
+
+let priceDisplay = ticketPrice.toFixed(2);
+
+
